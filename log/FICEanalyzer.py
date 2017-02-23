f94cd5b731c76262d1f6b5cb16c3b96ff745573d
adrobiso <adrobiso@outlook.com>
2017-02-22 21:08:25 -0800

    updated edited AI's

--- log/FICEanalyzer.py
+++ log/FICEanalyzer.py
@@ -8,7 +8,8 @@ This is a temporary script file.
 import os
 
 roundCount = 0
-winCount = 0
+p1winCount = 0
+p2winCount = 0
 tieCount = 0
 
 for filename in os.listdir("./point"):
@@ -18,9 +19,12 @@ for filename in os.listdir("./point"):
                 round = line.split(",")
                 roundCount = roundCount + 1
                 if (int(round[1]) > int(round[2])):
-                    winCount = winCount + 1
+                    p1winCount = p1winCount + 1
                 elif (int(round[1]) == int(round[2])):
                     tieCount = tieCount + 1
+                else:
+                    p2winCount = p2winCount + 1
+                        
         
-print("rounds: %d\nwins: %d\nties: %d" % (roundCount, winCount, tieCount))
+print("rounds: %d\np1 wins: %d\np2 wins: %d\nties: %d" % (roundCount, p1winCount, p2winCount, tieCount))
 input()
